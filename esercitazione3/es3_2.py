ade93e113289f13f6f30be77329aee6a4e666569
mari <mari@ubuntu.vm.myguest.virtualbox.org>
2022-10-21 18:21:05 +0200

    esercizio iniziato a casa

---
+++ esercitazione3/es3_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+tabella = pd.read_csv('vel_vs_time.csv')
+print(tabella)
+
+'''
+tempi = tabella[t]
+vel = tabella[v]
+
+print(tempi)
+print(vel)
+'''
